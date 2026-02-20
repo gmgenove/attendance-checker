77cd1b3f696897dd669cfd4a44ee10a821eeae94
gmgenove <960207+gmgenove@users.noreply.github.com>
2026-02-20 23:12:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1043,7 +1043,7 @@ async function generateStudentMatrixPDF(pdfDoc, student, sid, subjects, sem, fon
         page.drawText('A', { x: totalX + 30, y, size: 7, font: bold });
         page.drawText('%', { x: totalX + 45, y, size: 7, font: bold });
 		y -= 15; // Space for the rotated dates
-        page.drawLine({ start: { x: 20, y: y + 5 }, end: { x: 990, y: y + 5 }, thickness: 1 });
+        page.drawLine({ start: { x: 30, y: y + 5 }, end: { x: 990, y: y + 5 }, thickness: 1 });
 
         // 3. DRAW SUBJECT ROWS
         groupMembers.forEach((sub, index) => {
