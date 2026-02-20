26ccf9a2708b5ec7d065e36b19ad6696fb6331dd
gmgenove <960207+gmgenove@users.noreply.github.com>
2026-02-20 22:48:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1038,12 +1038,12 @@ async function generateStudentMatrixPDF(pdfDoc, student, sid, subjects, sem, fon
         });
 
         const totalX = startX + (Math.min(sortedDates.length, 35) * colWidth) + 5;
-        page.drawText('P', { x: totalX + 5, y, size: 7, font: bold });
-        page.drawText('L', { x: totalX + 20, y, size: 7, font: bold });
-        page.drawText('A', { x: totalX + 35, y, size: 7, font: bold });
-        page.drawText('%', { x: totalX + 55, y, size: 7, font: bold });
+        page.drawText('P', { x: totalX, y, size: 7, font: bold });
+        page.drawText('L', { x: totalX + 10, y, size: 7, font: bold });
+        page.drawText('A', { x: totalX + 25, y, size: 7, font: bold });
+        page.drawText('%', { x: totalX + 40, y, size: 7, font: bold });
 		y -= 45; // Space for the rotated dates
-        page.drawLine({ start: { x: 20, y: y + 5 }, end: { x: 990, y: y + 5 }, thickness: 1 });
+        page.drawLine({ start: { x: 5, y: y + 5 }, end: { x: 990, y: y + 5 }, thickness: 1 });
 
         // 3. DRAW SUBJECT ROWS
         groupMembers.forEach((sub, index) => {
@@ -1081,10 +1081,10 @@ async function generateStudentMatrixPDF(pdfDoc, student, sid, subjects, sem, fon
                 const totalPossible = sortedDates.length - excused;
                 const perc = totalPossible > 0 ? Math.round((presentTotal / totalPossible) * 100) : 0;
 
-                page.drawText(`${c.P || 0}`, { x: totalX + 5, y, size: 7, font });
-                page.drawText(`${c.L || 0}`, { x: totalX + 20, y, size: 7, font });
-                page.drawText(`${c.A || 0}`, { x: totalX + 35, y, size: 7, font });
-                page.drawText(`${perc}%`, { x: totalX + 55, y, size: 7, font: bold });
+                page.drawText(`${c.P || 0}`, { x: totalX, y, size: 7, font });
+                page.drawText(`${c.L || 0}`, { x: totalX + 10, y, size: 7, font });
+                page.drawText(`${c.A || 0}`, { x: totalX + 25, y, size: 7, font });
+                page.drawText(`${perc}%`, { x: totalX + 40, y, size: 7, font: bold });
             }
             page.drawLine({ start: { x: 20, y: y - 2 }, end: { x: 990, y: y - 2 }, thickness: 0.1, color: rgb(0.8, 0.8, 0.8) });
         });
