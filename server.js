0f3783c9f3736e81d0daa876f628ab4ee393273b
gmgenove <960207+gmgenove@users.noreply.github.com>
2026-02-28 16:14:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
+console.log("DB URL IS:", process.env.DATABASE_URL
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL//, ssl: { rejectUnauthorized: false }
 });
